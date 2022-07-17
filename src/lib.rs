6381bde34419b693199b107cb6fde48a6994aeb9
Kaizen Sparc <kaizenwoof@integralement.nu>
2022-07-17 17:08:08 +0200

    Add lib and bin with systemd service

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,84 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{Read, Seek, Write};
+
+use anyhow::Result;
+
+fn read_temperature(file: &mut std::fs::File) -> Result<f32> {
+    let mut buf: Vec<u8> = vec![];
+    file.rewind()?;
+    file.read_to_end(&mut buf)?;
+    std::str::from_utf8(&buf)?
+        .trim()
+        .parse::<u32>()
+        .map_err(|e| anyhow::anyhow!("Cannot read temperature: {:?}", e))
+        .map(|i| i as f32 / 1000.0f32)
+}
+
+pub struct FanController {
+    fan: std::fs::File,
+    temperatures: Vec<std::fs::File>,
+    high_temp: f32,
+    low_temp: f32,
+}
+
+impl FanController {
+    pub fn new(
+        high_temp_deg: f32,
+        low_temp_deg: f32,
+        fan_path: std::path::PathBuf,
+        temperatures: Vec<std::path::PathBuf>,
+    ) -> Result<Self> {
+        Ok(FanController {
+            fan: std::fs::File::options()
+                .write(true)
+                .read(false)
+                .open(fan_path)
+                .map_err(|e| anyhow::anyhow!("Could not open fan file: {:?}", e))?,
+            temperatures: temperatures
+                .into_iter()
+                .map(|p| {
+                    std::fs::File::options()
+                        .write(false)
+                        .read(true)
+                        .open(p)
+                        .map_err(|e| anyhow::anyhow!("Could not open temperature file: {:?}", e))
+                })
+                .collect::<Result<Vec<std::fs::File>>>()?,
+            high_temp: high_temp_deg,
+            low_temp: low_temp_deg,
+        })
+    }
+
+    pub fn get_mean_cpu_temp(&mut self) -> Result<f32> {
+        let temps = self
+            .temperatures
+            .iter_mut()
+            .map(|mut f| read_temperature(&mut f))
+            .collect::<Result<Vec<f32>>>()?;
+
+        Ok(temps.iter().sum::<f32>() / temps.len() as f32)
+    }
+
+    pub fn set_fan_off(&mut self) -> Result<()> {
+        self.fan
+            .write_all(&vec!['0' as u8])
+            .map_err(|e| anyhow::anyhow!("Could not turn off fan: {:?}", e))
+    }
+
+    pub fn set_fan_on(&mut self) -> Result<()> {
+        self.fan
+            .write_all(&vec!['1' as u8])
+            .map_err(|e| anyhow::anyhow!("Could not turn on fan: {:?}", e))
+    }
+
+    pub fn run_loop_once(&mut self) -> Result<()> {
+        let cpu_temp = self.get_mean_cpu_temp()?;
+
+        if cpu_temp > self.high_temp {
+            self.set_fan_on()
+        } else if cpu_temp < self.low_temp {
+            self.set_fan_off()
+        } else {
+            Ok(())
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+use xu4fan::FanController;
+
+/// Simple program to control the CPU fan of the odroid XU4
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Hysteresis higher temperature
+    #[clap(short, long, value_parser, default_value_t = 60.0)]
+    high_temp: f32,
+
+    /// Hysteresis lower temperature
+    #[clap(short, long, value_parser, default_value_t = 50.0)]
+    low_temp: f32,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let mut fc = FanController::new(
+        args.high_temp,
+        args.low_temp,
+        std::path::Path::new("/sys/devices/virtual/thermal/cooling_device2/cur_state")
+            .to_path_buf(),
+        glob::glob("/sys/devices/virtual/thermal/thermal_zone*/temp")?
+            .map(|x| {
+                x.map(|p| p.to_path_buf())
+                    .map_err(|e| anyhow::anyhow!("Cannot open path: {:?}", e))
+            })
+            .collect::<Result<Vec<std::path::PathBuf>>>()?,
+    )?;
+
+    loop {
+        println!("It's currently {:?}C here", fc.get_mean_cpu_temp()?);
+        fc.run_loop_once()?;
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
 }
